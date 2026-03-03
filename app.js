c32ae418910732ca7fed121c41936d6dbd75c856
Nisarg Bathwar <nisargbathwar@Nisargs-MacBook-Air.local>
2026-03-03 21:12:09 +0530

    Added MVC structure with gitignore

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.json());
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected ✅"))
+  .catch(err => console.log("DB Error:", err));
 
 app.get("/", (req, res) => {
   res.send("Ecommerce Backend Running 🚀");
