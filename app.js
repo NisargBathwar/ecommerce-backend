4523206a59370134af49ed4ca8c22a3f7a7dd735
Nisarg Bathwar <nisargbathwar@Nisargs-MacBook-Air.local>
2026-03-03 21:16:05 +0530

    Added Student MVC code

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 
+const studentRoutes = require("./routes/studentroute");
+
 const app = express();
 app.use(express.json());
 
-// MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected ✅"))
   .catch(err => console.log("DB Error:", err));
 
+app.use("/students", studentRoutes);
+
 app.get("/", (req, res) => {
-  res.send("Ecommerce Backend Running 🚀");
+  res.send("Student API Running 🚀");
 });
 
 app.listen(3000, () => {
---
+++ controllers/studentcontroller.js
@@ -0,0 +1,22 @@
+const Student = require("../models/students");
+
+// Create Student
+exports.createStudent = async (req, res) => {
+  try {
+    const student = new Student(req.body);
+    const savedStudent = await student.save();
+    res.status(201).json(savedStudent);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Get All Students
+exports.getStudents = async (req, res) => {
+  try {
+    const students = await Student.find();
+    res.json(students);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
